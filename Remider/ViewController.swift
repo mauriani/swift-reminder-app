e10f6aed28e2744f538c6d6f3990e3bee23c2bda
mauriani <maurianim@gmail.com>
2025-01-18 21:54:26 -0300

    Initial Commit

---
+++ Remider/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Remider
+//
+//  Created by Mauriani Maciel Lima on 18/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ RemiderTests/RemiderTests.swift
@@ -0,0 +1,17 @@
+//
+//  RemiderTests.swift
+//  RemiderTests
+//
+//  Created by Mauriani Maciel Lima on 18/01/25.
+//
+
+import Testing
+@testable import Remider
+
+struct RemiderTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
