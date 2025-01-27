bd294acfc220e00b6e4751505a19da3a4bc2016f
mauriani <maurianim@gmail.com>
2025-01-27 11:32:04 -0300

    add bottom sheet

---
+++ Remider/sources/Features/LoginBottomSheetViewController/LoginBottomSheetViewController.swift
@@ -0,0 +1,59 @@
+//
+//  LoginBottomSheetViewController.swift
+//  Remider
+//
+//  Created by Mauriani Maciel Lima on 27/01/25.
+//
+
+import Foundation
+import UIKit
+
+class LoginBottomSheetViewController: UIViewController {
+    let loginView = LoginBottomSheetView()
+    var handleAreaHeight: CGFloat = 50.0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setupUI()
+        setupGesture()
+    }
+    
+    private func setupUI() {
+        self.view.addSubview(loginView)
+        loginView.translatesAutoresizingMaskIntoConstraints = false
+        
+        setupConstraints()
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            loginView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            loginView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            loginView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+        ])
+        
+        let heightConstraint = loginView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.5).isActive = true
+    }
+    
+    private func setupGesture() {
+        //proxima aula
+    }
+    
+    private func handlePanGesture() {
+        //proxima aula
+    }
+    
+    func animateShow(completion: (() -> Void)? = nil) {
+        // da update no layout
+        self.view.layoutIfNeeded()
+        // pega a altyra da pagina de login
+        loginView.transform = CGAffineTransform(translationX: 0, y: loginView.frame.height)
+        UIView.animate(withDuration: 0.3, animations: {
+            self.loginView.transform = .identity
+            self.view.layoutIfNeeded()
+        }) { _ in
+            completion?()
+        }
+    }
+}
--- Remider/sources/Features/Splash/SplashViewController.swift
+++ Remider/sources/Features/Splash/SplashViewController.swift
@@ -25,6 +25,7 @@ class SplashViewController: UIViewController {
         self.view.backgroundColor = Colors.primaryRedBase
         
         setupConstraints()
+        setupGesture()
     }
     
     // fala onde os elementos estao
@@ -39,5 +40,22 @@ class SplashViewController: UIViewController {
         contentView.translatesAutoresizingMaskIntoConstraints = false
         
     }
+    
+    private func setupGesture() {
+        // armazena o gesto
+          let tapGesture = UITapGestureRecognizer(target: self, action: #selector(showLoginBottomSheet))
+          self.view.addGestureRecognizer(tapGesture)
+      }
+      
+    
+    @objc
+        private func showLoginBottomSheet() {
+            let loginBottomSheet = LoginBottomSheetViewController()
+            loginBottomSheet.modalPresentationStyle = .overCurrentContext
+            loginBottomSheet.modalTransitionStyle = .crossDissolve
+            self.present(loginBottomSheet, animated: false) {
+                loginBottomSheet.animateShow()
+            }
+        }
     
 }
